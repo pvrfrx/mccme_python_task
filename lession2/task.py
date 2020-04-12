7af22d6f3a3b12cd61997e351e34e6cf3934f504
pvrfrx <pvrfrx@gmail.com>
2020-04-12 21:39:14 +0300

    second commit

--- lession2/task.py
+++ lession2/task.py
@@ -1,1 +1,6 @@
-print(abs(complex(179, 971)))
+i = 0
+string = ''
+while i < 50:
+    string = string + '179'
+    i = i + 1
+print(string. ** 2)
---
+++ lession2/task4.py
@@ -0,0 +1,6 @@
+i = 0
+string = ''
+while i < 100:
+    string = string + 'A'
+    i = i + 1
+print(string)
